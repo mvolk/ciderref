5a555e9fa62019a9147a59c8155a55b54db5be2a
Michael Volk <michael@volksys.com>
2016-06-11 22:33:04 -0700

    Make onRequestHome optional and don't bind to logo's onClick if not present.

--- app/components/PageHeader.jsx
+++ app/components/PageHeader.jsx
@@ -6,9 +6,22 @@ export default class PageHeader extends React.Component {
       <div className="row">
         <div className="col-xs-12 h3 page-header">
           <img src="images/icon-gear-48.png" className="settings-icon link" onClick={this.props.onOpenSettings} />
-          <div className="link" onClick={this.props.onRequestHome}><img src="images/apple.png" className="logo-icon"/>CiderRef</div>
+          {this.getLogo()}
         </div>
       </div>
     );
+  }
+
+  getLogo() {
+    if (this.props.onRequestHome) {
+      return (
+        <div className="link" onClick={this.props.onRequestHome}>
+          <img src="images/apple.png" className="logo-icon"/>CiderRef
+        </div>
+      );
+    } else {
+      return <div><img src="images/apple.png" className="logo-icon"/>CiderRef</div>
+    }
+
   }
 }

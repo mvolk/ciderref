1318d314e7e54957b149661ac03f984f97d2184d
Michael Volk <michael@volksys.com>
2016-06-26 12:17:21 -0700

    Simplify SpecificGravityInput

--- app/components/SpecificGravityInput.jsx
+++ app/components/SpecificGravityInput.jsx
@@ -25,27 +25,21 @@
 import React from 'react';
 
 export default class SpecificGravityInput extends React.Component {
-  constructor (props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
   render () {
     return (
       <input
         type="number"
         value={this.props.value}
-        onChange={this.handleChange}
+        onChange={(e) => {
+          e.stopPropagation();
+          this.props.onChange(Number(event.target.value));
+        }}
         autoFocus={this.props.autoFocus || false}
         min={this.props.minValue || '0.990'}
         max={this.props.maxValue || '1.100'}
         step={this.props.step || '0.001'}
       />
     );
-  }
-
-  handleChange (event) {
-    this.props.onChange(Number(event.target.value));
   }
 }
 
